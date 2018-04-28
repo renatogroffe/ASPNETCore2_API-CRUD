b1ddf2b1146253aba552cd53a54d80ae7026df47
Renato Groffe <renato.groffe@yahoo.com.br>
2018-04-28 10:48:42 -0300

    ASP.NET Core 2.0 + Entity Framework Core InMemory
    ASP.NET Core 2.0 + Entity Framework Core InMemory

---
+++ APIProdutos/APIProdutos/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using APIProdutos.Data;
+using APIProdutos.Business;
+
+namespace APIProdutos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseInMemoryDatabase("InMemoryDatabase"));
+            services.AddScoped<ProdutoService>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
